f7a0aaea486ec71780492853a968326e8079f02a
liuxi_learnning <liuxi_aliyun_0728>
2020-04-21 15:36:31 +0800

    test params

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -8,6 +8,11 @@
 </head>
 <body>
 
+<a href="testParams?username=tom&age=11">Test Params</a>
+<br><br>
+<form action="testMethod" method="post">
+<input type="submit" value="test post">
+</form>
 <br><br>
 <a href="hello">hello</a>
 </body>
--- src/com/liuxi/springmvc/handler/HelloWorldHandler.java
+++ src/com/liuxi/springmvc/handler/HelloWorldHandler.java
@@ -2,11 +2,25 @@ package com.liuxi.springmvc.handler;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloWorldHandler {
     
     private static final String SUCCESS = "success";
+    
+    @RequestMapping(value="/testParams", params= {"username", "age!=10"})
+    public String testParams() {
+        System.out.println("testParams");
+        return SUCCESS;
+    }
+    
+    @RequestMapping(value="/testMethod", method=RequestMethod.POST)
+    public String testMethod() {
+        
+        System.out.println("test method");
+        return SUCCESS;
+    }
     
     @RequestMapping("/hello")
     public String hello() {
